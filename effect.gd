a11e4532dcd533c61f332e90aeb355a82fc20197
Swyfte <arianwenpaganuzzi@gmail.com>
2025-04-16 14:39:50 +0100

    Created effect class base

---
+++ effect.gd
@@ -0,0 +1,15 @@
+class_name Effect
+
+extends Node
+
+enum TriggerTime {TURN_START, TURN_END}
+
+@export var effectName: String
+@export var description: String
+@export var magnitude: int:
+	set(value):
+		magnitude = clamp(value, 0, 3)
+@export var effectTrigger: TriggerTime
+
+func trigger():
+	pass
