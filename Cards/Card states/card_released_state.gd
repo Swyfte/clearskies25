80a035e0437c6416551f01f71c589637649048e7
Swyfte <arianwenpaganuzzi@gmail.com>
2025-04-17 16:07:31 +0100

    Created card dragging mechanics
    Followed a tutorial, fairly happy with this

---
+++ Cards/Card states/card_released_state.gd
@@ -0,0 +1,16 @@
+extends CardState
+
+var played: bool
+
+func enter() -> void:
+	card_ui.colour.color = Color.DARK_VIOLET
+	card_ui.state.text = "RELEASED"
+	played = false
+	if not card_ui.targets.is_empty():
+		played = true
+		print("Play card for target(s): ", card_ui.targets)
+
+func on_input(event: InputEvent) -> void:
+	if played:
+		return
+	transition_requested.emit(self, CardState.State.BASE)
---
+++ Cards/Card states/card_state_machine.gd
@@ -0,0 +1,46 @@
+class_name CardStateMachine
+extends Node
+
+@export var initialState: CardState
+
+var currentState: CardState
+var states := {}
+
+func init(card: CardUI) -> void:
+	for child in get_children():
+		if child is CardState:
+			states[child.state] = child
+			child.transition_requested.connect(on_transition_requested)
+			child.card_ui = card
+	if initialState:
+		initialState.enter()
+		currentState = initialState
+
+func on_input(event: InputEvent) -> void:
+	if currentState:
+		currentState.on_input(event)
+
+func on_gui_input(event: InputEvent) -> void:
+	if currentState:
+		currentState.on_gui_input(event)
+
+func on_mouse_entered() -> void:
+	if currentState:
+		currentState.on_mouse_entered()
+
+func on_mouse_exit() -> void:
+	if currentState:
+		currentState.on_mouse_exit()
+
+func on_transition_requested(from: CardState, to: CardState.State):
+	if from != currentState:
+		return
+	var newState: CardState = states[to]
+	if not newState:
+		return
+	
+	if currentState:
+		currentState.exit()
+	
+	newState.enter()
+	currentState = newState
